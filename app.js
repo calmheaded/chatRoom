3f773d17767354bdbe72715ad81474259b412afa
1991xiao831 <271295492@qq.com>
2020-10-22 21:59:30 +0800

    20201022

--- app.js
+++ app.js
@@ -32,16 +32,16 @@
 // //     socket.on('message', function (msg) {
 // //         console.log('receive messge : ' + msg );
 // //     });
-    
+
 // //     //发送消息
 // //     socket.emit('message', 'hello');
-    
+
 // //     //断开连接回调
 // //     socket.on('disconnect', function () { 
 // //         console.log('socket disconnect');
 // //     });
 // //  });
- 
+
 // http.listen(port, () => console.log(`Example11 app listening on port ${port}!`))
 
 
@@ -166,7 +166,7 @@
 //   })
 
 
-  
+
 // })
 
 
@@ -201,36 +201,36 @@ var io = require('socket.io')(server);
 server.listen(8080);
 
 app.get('/', function (req, res) {
-  res.sendFile (__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 // app.get('/', (req, res) => res.send('Hello World!222'))
 
 
 //在线用户
-var onlineUser={};
-var onlineCount=0;
+var onlineUser = {};
+var onlineCount = 0;
 
 io.on('connection', function (socket) {
     console.log('新用户登录');
-     //监听新用户加入
-    socket.on('login',function (obj) {
-        console.log(obj,'obj')
-        socket.name=obj.userid;
-          //检查用户在线列表
-          if(!onlineUser.hasOwnProperty(obj.userid)){
-            onlineUser[obj.userid]=obj.username;
+    //监听新用户加入
+    socket.on('login', function (obj) {
+        console.log(obj, 'obj')
+        socket.name = obj.userid;
+        //检查用户在线列表
+        if (!onlineUser.hasOwnProperty(obj.userid)) {
+            onlineUser[obj.userid] = obj.username;
             //在线人数+1
             onlineCount++;
         }
         //广播消息
-        io.emit('login',{onlineUser:onlineUser,onlineCount:onlineCount,user:obj});
-        console.log(obj.username+"加入了聊天室");
+        io.emit('login', { onlineUser: onlineUser, onlineCount: onlineCount, user: obj });
+        console.log(obj.username + "加入了聊天室");
     })
-  
-     //监听用户发布聊天内容
-     socket.on('message', function(obj){
+
+    //监听用户发布聊天内容
+    socket.on('message', function (obj) {
         //向所有客户端广播发布的消息
         io.emit('message', obj);
-        console.log(obj.username+'说：'+obj.content);
+        console.log(obj.username + '说：' + obj.content);
     });
 });
